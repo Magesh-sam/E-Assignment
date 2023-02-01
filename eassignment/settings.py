1002671e87e1db7abfe59f0148c9fdc353104c42
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-02-01 12:01:37 +0530

    updated setting

--- eassignment/settings.py
+++ eassignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pbba(5k6!%#nt()9i-e7)1)+sznspe#-jed2!6bgka!)8gb03j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
